f361082699427a47d1390d15f7e488730b182a27
lnvlenovobrick-cloud <lnv.le.no.vo.brick@gmail.com>
2026-06-27 20:44:29 +0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,18 @@
+import { useState, useEffect } from 'react';
 import Error from 'next/error';
 import { useTheme } from '@lib/context/theme-context';
 import { SEO } from '@components/common/seo';
 
 export default function NotFound(): JSX.Element {
   const { theme } = useTheme();
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const isDarkMode = ['dim', 'dark'].includes(theme);
+  useEffect(() => {
+    // Only checks the theme once safely loaded in the user's browser window
+    if (theme) {
+      setIsDarkMode(['dim', 'dark'].includes(theme));
+    }
+  }, [theme]);
 
   return (
     <>
