5cd5203cc91c4b172e971f12c68a9f25a204dc47
lnvlenovobrick-cloud <lnv.le.no.vo.brick@gmail.com>
2026-06-27 21:15:41 +0300

    Bleh

--- customlib/firebase/collections.ts
+++ customlib/firebase/collections.ts
@@ -4,26 +4,26 @@ import { tweetConverter } from '@lib/types/tweet';
 import { bookmarkConverter } from '@lib/types/bookmark';
 import { statsConverter } from '@lib/types/stats';
 import { db } from './app';
-import type { CollectionReference } from 'firebase/firestore';
+import type { CollectionReference, Firestore } from 'firebase/firestore';
 import type { Bookmark } from '@lib/types/bookmark';
 import type { Stats } from '@lib/types/stats';
 
-export const usersCollection = collection(db, 'users').withConverter(
+export const usersCollection = collection(db as Firestore, 'users').withConverter(
   userConverter
 );
 
-export const tweetsCollection = collection(db, 'tweets').withConverter(
+export const tweetsCollection = collection(db as Firestore, 'tweets').withConverter(
   tweetConverter
 );
 
 export function userBookmarksCollection(
   id: string
 ): CollectionReference<Bookmark> {
-  return collection(db, `users/${id}/bookmarks`).withConverter(
+  return collection(db as Firestore, `users/${id}/bookmarks`).withConverter(
     bookmarkConverter
   );
 }
 
 export function userStatsCollection(id: string): CollectionReference<Stats> {
-  return collection(db, `users/${id}/stats`).withConverter(statsConverter);
+  return collection(db as Firestore, `users/${id}/stats`).withConverter(statsConverter);
 }
